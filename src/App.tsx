56bc8848000dd56abae592cbe4813cd1aeda5010
Alex <87208475+PuSh0chek@users.noreply.github.com>
2024-03-04 11:32:20 +0300

    Добавит настройки prettier и eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,85 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
 
 type Post = {
-  userId:number,
-  id:number,
-  title:string,
-  body:string,
+    userId: number
+    id: number
+    title: string
+    body: string
 }
 
 const API_URL = 'https://jsonplaceholder.typicode.com/posts'
 
-const MAX_POSTS = 10
+const App = () => {
+    const [posts, setPosts] = useState<Post[]>([])
+    const [page, setPage] = useState(1)
 
-function App() {
+    useEffect(() => {
+        function getData() {
+            axios
+                .get(`${API_URL}?_page=${page}&_per_page`)
+                .then(({ data }: { data: Post[] }) =>
+                    setPosts(data),
+                )
+        }
+        getData()
+    }, [page])
 
-  const [posts, setPosts] = useState<Post[]>([])
-  const [page, setPage] = useState(1)
-
-  useEffect(() => {
-    function getData() {
-      axios.get(`${API_URL}?_page=${page}&_per_page`)
-      .then(({data}:{data:Post[]}) => setPosts(data))
-    }
-    getData()
-  }, [page])
-
-  return (
-    <div className="App">
-      <h1>Get content</h1>
-      <div className='container-content'>
-        <ul>
-          {posts.map((post) => <li key={post.id}>{post.id}</li> )}
-        </ul>
-      </div>
-      <div style={{display:'flex', justifyContent: 'center'}}>
-        {page > 1 && <button className="button" onClick={() => setPage(page - 1)}>Previous</button> }
-        <p>Current page {page}</p>
-        {page < posts.length && <button className="button" onClick={() => setPage(page + 1)}>Next</button> }
-      </div>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <h1>Get content</h1>
+            <div className='container-content'>
+                <ul>
+                    {posts.map((post) => (
+                        <li key={post.id}>{post.id}</li>
+                    ))}
+                </ul>
+            </div>
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                }}
+            >
+                {page !== 1 && (
+                    <button
+                        className='button'
+                        onClick={() => setPage(1)}
+                    >
+                        First
+                    </button>
+                )}
+                {page > 1 && (
+                    <button
+                        className='button'
+                        onClick={() => setPage(page - 1)}
+                    >
+                        Previous
+                    </button>
+                )}
+                <p>Current page {page}</p>
+                {page < posts.length && (
+                    <button
+                        className='button'
+                        onClick={() => setPage(page + 1)}
+                    >
+                        Next
+                    </button>
+                )}
+                {page !== posts.length && (
+                    <button
+                        className='button'
+                        onClick={() =>
+                            setPage(posts.length)
+                        }
+                    >
+                        Last
+                    </button>
+                )}
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
